5e95a3d0033183d55fef7a456aa8f57f6978fca0
Sadman Sakib <ssakib4040@gmail.com>
2025-09-20 20:27:18 +0600

    ip added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,55 @@
-export default function Home() {
-  return <div>Home</div>;
+"use client";
+
+import type { PutBlobResult } from "@vercel/blob";
+import { useState, useRef } from "react";
+
+export default function AvatarUploadPage() {
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = useState<PutBlobResult | null>(null);
+  return (
+    <>
+      <h1>Upload Your Avatar</h1>
+
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          if (!inputFileRef.current?.files) {
+            throw new Error("No file selected");
+          }
+
+          const file = inputFileRef.current.files[0];
+
+          const response = await fetch(
+            `/api/avatar/upload?filename=${file.name}`,
+            {
+              method: "POST",
+              body: file,
+            }
+          );
+
+          // get the upload progress
+            
+
+          const newBlob = (await response.json()) as PutBlobResult;
+
+          setBlob(newBlob);
+        }}
+      >
+        <input
+          name="file"
+          ref={inputFileRef}
+          type="file"
+          accept="image/jpeg, image/png, image/webp"
+          required
+        />
+        <button type="submit">Upload</button>
+      </form>
+      {blob && (
+        <div>
+          Blob url: <a href={blob.url}>{blob.url}</a>
+        </div>
+      )}
+    </>
+  );
 }
